bc9c3d373638a97e3256e1c81be7dd1420f1be88
SpringLee <1028789852@qq.com>
2017-03-29 10:27:16 +0800

    本地提交

--- 120ask.py
+++ 120ask.py
@@ -67,7 +67,7 @@ dederesult= dedetable.col_values(1)
 print(" Success！")
 
 con = pymysql.connect(user='root', password='root', database='ask120',host='127.0.0.1',charset='utf8')
-
+cur =con.cursor()
 
 print("\r")
 print(" 正在获取采集URl......")
